57b5ee86e13d5550e24336302b4be5752a04a535
Nick <nick@studiocoda.com>
2014-03-21 09:22:50 -0700

    thredis. new module.

---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+requires = [
+    'redis',
+    ]
+
+setup(name='thredis',
+      version='0.2',
+      description='Primitive Models and Thread Safe Redis Classes',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      author='adoc',
+      author_email='adoc@code.webmob.net',
+      url='https://github.com/adoc',
+      keywords='redis threadsafe model',
+      packages=['thredis'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      tests_require=requires,
+      test_suite="tests",
+      )
